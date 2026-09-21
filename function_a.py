56690bfde2284effa7422708d42c55740f2e2328
sh0stik <dar.shostak@gmail.com>
2026-09-21 10:30:52 -0700

    Implement most_common_value

--- function_a.py
+++ function_a.py
@@ -1,8 +1,18 @@
 def most_common_value(number_list):
     """ returns the most common element of the list
     """
-    pass
+    nums_counts = {}
+    for num in number_list:
+        nums_counts[num] = nums_counts.get(num, 0) + 1
 
+    result = number_list[0]
+    max_count = 0
+    for num, count in nums_counts.items():
+        if count > max_count:
+            max_count = count
+            result = num
+
+    return result
 
 if __name__ == "__main__":
     nums = [1, 1, 3, 3, 3, 7, 8, 2, 1, 3]
